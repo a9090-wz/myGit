c1aad381da18896d0f3a923158d3fccb19dfa00c
wangzhen <1228161782@qq.com>
2021-08-20 11:28:45 +0800

    修改test01.java

--- src/main/java/com/git/test01.java
+++ src/main/java/com/git/test01.java
@@ -3,5 +3,10 @@ package com.git;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("test01...");
+        System.out.println("test02...");
+        System.out.println("test03...");
+        System.out.println("test04...");
+
+
     }
 }
